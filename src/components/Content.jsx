c7a861962b4944dbd98c83a3e19228420b63b283
theHabesa1 <yaredabera215@gmail.com>
2023-09-09 09:28:02 +0300

    sahma init

---
+++ src/components/Content.jsx
@@ -0,0 +1,256 @@
+import React from 'react'
+import {  
+    Grid, 
+    Typography,
+    IconButton,
+    Card,
+    CardContent,
+} from "@mui/material";
+// icons
+import SettingsSystemDaydreamIcon from '@mui/icons-material/SettingsSystemDaydream';
+import LocalParkingIcon from '@mui/icons-material/LocalParking';
+import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
+import FastfoodOutlinedIcon from '@mui/icons-material/FastfoodOutlined';
+import PoolOutlinedIcon from '@mui/icons-material/PoolOutlined';
+import WifiPasswordIcon from '@mui/icons-material/WifiPassword';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import NewspaperIcon from '@mui/icons-material/Newspaper';
+// components
+import Title from './Title'
+import Paragraph from './Paragraph'
+
+
+const Content = () => {
+  return (    
+        <Grid container spacing={0}   
+        sx={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'center',
+            alignItems: 'center',
+            py: 10,
+            px: 2,
+        }}
+        >
+            <Grid item xs={12} sm={12} md={5}
+            component = 'section'
+            >
+                <Title
+                text={
+                    'What we are offering?'
+                }
+                textAlign={'start'}
+                />
+
+                <Typography 
+                variant='h6'
+                component='h4' 
+                sx = {{
+                    fontWeight: '400',
+                    paddingTop: 1,
+                }}
+                >
+                    Property facilities
+                </Typography>
+
+                <Paragraph 
+                text={
+                    ' We work with more than 100+ clients\
+                    and our customers trust on out quality\
+                    product and trust own our product.'
+                }
+                maxWidth = {'75%'}
+                mx={0}
+                textAlign={'start'}
+                />
+            </Grid>
+            
+            <Grid item xs={12} sm={6} md={3}>
+                <Card 
+                square={ true }
+                sx={{
+                    minHeight: 200,
+                    display: 'flex',
+                    flexDirection:'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                    border: '1px solid #ccc',
+                }}>
+                    <CardContent>
+                        <IconButton>
+                            <MenuBookIcon 
+                            fontSize="large"
+                            color="primary" />
+                        </IconButton>
+                        <Typography 
+                        variant="h5" 
+                        component="p"
+                        sx={{
+                            fontWeight: 700,
+                            textTransform: 'capitalize',
+                        }}
+                        >
+                        Digital menu
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={3}>
+                <Card 
+                square={ true }
+                sx={{ 
+                    minHeight: 200,
+                    display: 'flex',
+                    flexDirection:'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center', 
+                    border: '1px solid #ccc'
+                }}>
+                    <CardContent>
+                        <IconButton>
+                            <SettingsSystemDaydreamIcon 
+                            fontSize="large"
+                            color="primary" />
+                        </IconButton>
+                        <Typography 
+                        variant="h5" 
+                        component="p"
+                        sx={{
+                            fontWeight: 700,
+                            textTransform: 'capitalize',
+                        }}
+                        >
+                        ERP system
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={2}
+            sx={{
+                display: {xs: 'none', sm: 'block'},
+            }}  
+            >
+                <Card 
+                square={ true }
+                sx={{ 
+                    boxShadow: 'none',
+                    minHeight: 180,
+                    display: 'flex',
+                    flexDirection:'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center',
+                }}>
+                    <CardContent>
+                        <ArrowCircleRightRoundedIcon
+                        fontSize="large"
+                        color="primary" />
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            <Grid item xs={12} sm={6} md={3}>    
+                <Card 
+                square={ true }
+                sx={{ 
+                    minHeight: 200,
+                    display: 'flex',
+                    flexDirection:'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center', 
+                    border: '1px solid #ccc'
+                }}>
+                    <CardContent>
+                        <IconButton>
+                            <NewspaperIcon
+                            fontSize="large"
+                            color="primary" />
+                        </IconButton>
+                        <Typography 
+                        variant="h5" 
+                        component="p"
+                        sx={{
+                            fontWeight: 700,
+                            textTransform: 'capitalize',
+                        }}
+                        >
+                        Gazetta.com
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>
+
+            {/* <Grid item xs={12} sm={6} md={3}>
+                <Card 
+                square={ true }
+                sx={{ 
+                    minHeight: 200,
+                    display: 'flex',
+                    flexDirection:'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center',                    
+                    border: '1px solid #ccc',
+                }}
+                >
+                    <CardContent>
+                        <IconButton>
+                            <PoolOutlinedIcon 
+                            fontSize="large"
+                            color="primary" />
+                        </IconButton>
+                        <Typography 
+                        variant="h5" 
+                        component="p"
+                        sx={{
+                            fontWeight: 700,
+                            textTransform: 'capitalize',
+                        }}
+                        >
+                        swimming pool
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid> */}
+
+            {/* <Grid item xs={12} sm={6} md={3}>
+                <Card 
+                square={ true }
+                sx={{ 
+                    minHeight: 200,
+                    display: 'flex',
+                    flexDirection:'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    textAlign: 'center', 
+                    border: '1px solid #ccc',
+                }}>
+                    <CardContent>
+                        <IconButton>
+                            <WifiPasswordIcon
+                            fontSize="large"
+                            color="primary" />
+                        </IconButton>
+                        <Typography 
+                        variant="h5" 
+                        component="p"
+                        sx={{
+                            fontWeight: 700,
+                            textTransform: 'capitalize',
+                        }}
+                        >
+                        Internet
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </Grid>  */}
+        </Grid>
+    );
+}
+
+export default Content
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,100 @@
+import React from 'react'
+import {
+    AppBar,
+    Toolbar,
+    Box,
+    List,
+    ListItem,
+    Typography, 
+    styled,
+    ListItemButton,
+    ListItemText,
+} from '@mui/material';
+// menu
+import DrawerItem from './DrawerItem';
+// rotas
+import { Link } from 'react-router-dom';
+import logo from "../assets/shama.png"
+
+// personalizacao
+const StyledToolbar = styled(Toolbar) ({
+    display: 'flex',
+    justifyContent: 'space-between',
+});
+
+const ListMenu = styled(List)(({ theme }) => ({
+    display: 'none',
+    [theme.breakpoints.up("sm")] : {
+        display: "flex",
+    },
+}));
+
+//rotas
+const itemList = [
+    {
+      text: "Home",
+      to: "/" 
+    },
+    {
+      text: "About",
+      to: "/about"
+    },
+    {
+        text: "Contact",
+        to: "/contact"
+    }
+];
+
+
+const Navbar = () => {
+    
+    return (
+        <AppBar 
+        component="nav" 
+        position="sticky"
+        sx={{ 
+            backgroundColor: '#ffffff', 
+        }}
+        elevation={0}
+        >
+            <StyledToolbar>
+            <img
+  src={logo} // Assuming the assets folder is at the root level of your project
+  alt="HBSales Logo"
+  style={{
+    height: 'auto', // Adjust the height as needed to maintain the logo's aspect ratio
+    width: '50px', // Adjust the width as needed
+  }}
+/>
+
+                <Box sx={{display: { xs: 'block', sm: 'none' } }}>
+                    <DrawerItem /> 
+                </Box>
+                <ListMenu>
+                    {itemList.map( ( item ) => {
+                        const { text } = item;
+                        return(
+                            <ListItem key={text}>
+                                <ListItemButton component={Link} to={item.to}
+                              sx={{
+                                color: '#000',
+                                textDecoration: 'none', // Remove the default underline
+                                "&:hover": {
+                                    borderBottom: '2px solid #3632A8', // Add a white line underline on hover
+                                   
+                                }
+                            }}
+                            
+                                >
+                                    <ListItemText primary={text} />
+                                </ListItemButton>
+                            </ListItem>
+                        )
+                    })}
+                </ListMenu>
+            </StyledToolbar>
+        </AppBar>
+    )
+}
+
+export default Navbar;
